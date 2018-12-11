46b954f16bc20bcd792d000f6c8378c14aa94d31
Jonathan Alorda <jonathan.alorda@gmail.com>
2018-12-10 22:15:31 -0600

    Created store for Redux app.

---
+++ learn-redux/client/store.js
@@ -0,0 +1,23 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import the reducer
+import rootReducer from './reducers/index'
+
+import comments from './data/comments'
+import posts from './data/posts'
+
+// create an object for the dfault data
+const defaultState = {
+    posts,
+    comments
+}
+
+export const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store)
+
+
+
+
